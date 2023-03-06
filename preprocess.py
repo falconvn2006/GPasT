3c1fbdd58aa374db2d5682963be11eec009b90a4
Falcon <88769526+buckdan@users.noreply.github.com>
2023-03-06 11:47:02 +0000

    Completed the data processor and a small dataset

--- preprocess.py
+++ preprocess.py
@@ -2,7 +2,7 @@ import time
 import os
 
 MAX_CHAR_LENGTH = 512
-MIN_CHAR_LENGTH = 400
+MIN_CHAR_LENGTH = 256
 
 NEW_LINE_CHAR = "<N>"
 
@@ -15,24 +15,36 @@ for dirpath, dirnames, filenames in os.walk(dataset_path):
 
 print(len(full_paths))
 
+print("Removing old dataset")
+time.sleep(2)
+try:
+    os.remove("pascal_dataset_text_code.txt")
+except Exception as e:
+    print("Looks like there is no dataset skipping the deletion")
+
 with open("pascal_dataset_text_code.txt", "a") as f:
     for fpath in full_paths:
-        print(fpath)
-        data = open(fpath, "rb").read().decode("utf-8")
-        # print(data)
-        fd = data.replace("\n", NEW_LINE_CHAR)
+        try:
+            print(fpath)
+            data = open(fpath, "rb").read().decode("utf-8")
+            # print(data)
+            fd = data.replace('\n', NEW_LINE_CHAR)
 
-        if 100 < len(data) < MAX_CHAR_LENGTH:
+            if 100 < len(data) < MAX_CHAR_LENGTH:
 
-            f.write(fd + '\n')
+                f.write(fd + '\n')
 
-            break
-        else:
-            sd = fd.split(f"{NEW_LINE_CHAR}{NEW_LINE_CHAR}")
-            substring = ""
-            for split in sd:
-                substring += split + f"{NEW_LINE_CHAR}{NEW_LINE_CHAR}"
-                if MIN_CHAR_LENGTH <= len(substring) <= MAX_CHAR_LENGTH:
-                    f.write(substring + '\n')
-                    substring = ""
-            break
+            else:
+                sd = fd.split(f"{NEW_LINE_CHAR}{NEW_LINE_CHAR}")
+                substring = ""
+                for split in sd:
+                    substring += split + f"{NEW_LINE_CHAR}{NEW_LINE_CHAR}"
+                    if MIN_CHAR_LENGTH <= len(substring) <= MAX_CHAR_LENGTH:
+                        f.write(substring + '\n')
+                        substring = ""
+        
+        except Exception as e:
+            print(str(e))
+
+
+print("Dataset created!")
