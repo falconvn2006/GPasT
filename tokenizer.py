1334078c537af1542e60f58d1ad4027a299987e1
Falcon <88769526+buckdan@users.noreply.github.com>
2023-03-10 05:43:31 +0000

    Add more things

--- tokenizer.py
+++ tokenizer.py
@@ -1,21 +1,19 @@
 from tokenizers import ByteLevelBPETokenizer
 from transformers import GPT2Config, GPT2LMHeadModel, GPT2Tokenizer
 
-TRAINED = True
 data_paths = ["pascal_dataset_text_code.txt"]
 
-if not TRAINED:
-    tokenizer = ByteLevelBPETokenizer()
+tokenizer = ByteLevelBPETokenizer()
 
-    tokenizer.train(files=data_paths, vocab_size=52_000, min_frequency=2, special_tokens=[
-        "<S>",
-        "<pad>",
-        "</s>",
-        "<unk>",
-        "<mask>",
-    ])
+tokenizer.train(files=data_paths, vocab_size=52_000, min_frequency=2, special_tokens=[
+    "<S>",
+    "<pad>",
+    "</s>",
+    "<unk>",
+    "<mask>",
+])
 
-    tokenizer.save_model("tokenizer")
+tokenizer.save_model("tokenizer")
 
 
 tokenizer = GPT2Tokenizer.from_pretrained("tokenizer")
@@ -30,8 +28,8 @@ tokenizer.add_special_tokens({
 
 # Testing the Tokenizer
 #
-# inp = "writeln('Hello World'!);"
-# t = tokenizer.encode(inp)
-# print(t)
-# print(tokenizer.decode(t))
+inp = "writeln('Hello World'!);"
+t = tokenizer.encode(inp)
+print(t)
+print(tokenizer.decode(t))
 #
